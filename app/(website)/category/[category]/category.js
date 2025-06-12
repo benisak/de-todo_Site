adba95ba3295d5438b3a348ffa02ae18b347c7fb
CrisJurado13 <cristianj@dreamcode.io>
2025-06-12 15:43:10 -0500

    DT category page

--- app/(website)/category/[category]/category.js
+++ app/(website)/category/[category]/category.js
@@ -12,16 +12,15 @@ export default function Author(props) {
   }
 
   return (
-    <div className=" mb-10 mt-14 lg:gap-[55px] px-4 md:px-[160px]">
+     <div className="bg-[#FAFAFA] pt-14 pb-10 px-4 md:px-[160px] lg:gap-[55px]">
       <div className="flex flex-col items-center justify-center">
-        <h1 className="text-brand-primary text-3xl font-semibold tracking-tight dark:text-white lg:text-5xl lg:leading-tight">
+        <h1 className="font-nunito text-center text-Black-500 text-2xl md:text-4xl font-black">
           {title}
         </h1>
-        <p className="mt-1 text-gray-600">
-          {recipes.length} Productos
-        </p>
+        <p className="mt-1 text-gray-600">{recipes.length} Productos</p>
       </div>
-      <div className="mt-14 grid gap-10 md:grid-cols-2 lg:gap-10 xl:grid-cols-3">
+
+      <div className="mt-14 grid gap-10 md:grid-cols-2 lg:gap-10 xl:grid-cols-4">
         {recipes.map((post) => (
           <PostList
             pathPrefix={"blog"}
