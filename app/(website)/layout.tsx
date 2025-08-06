a52834455ad54d3e1df7566c9af27b86112cee15
CrisJurado13 <cristianj@dreamcode.io>
2025-08-06 17:02:41 -0500

    de-todo GTM

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -159,10 +159,10 @@ export default async function Layout({
         <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-PXGJ36HN');`
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WCVXDGGH');`
           }}
         />
         {/* End Google Tag Manager */}
@@ -175,7 +175,7 @@ export default async function Layout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PXGJ36HN"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WCVXDGGH"
             height="0" 
             width="0" 
             style={{ display: 'none', visibility: 'hidden' }}
